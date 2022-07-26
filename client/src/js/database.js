91e2c915763c1c284070b1a53598a6f53bbc54cb
Your Name <you@example.com>
2022-07-26 10:19:13 -0400

    added some logic

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,26 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (id, content) => {
+  console.log("we're in the database.");
+  const contentDb = await openDB("content", 1);
+  const tx = contentDb.transaction("content", "readwrite");
+  const store = tx.objectStore("content");
+  const request = store.objectStore.put({ id: id, value: content });
+  const result = await request;
+  console.log("result.value", result);
+};
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  const contentDb = await openDB("content", 1);
+  const tx = contentDb.transaction("content", "readonly");
+  const store = tx.objectStore("content");
+  const request = store.objectStore.getAll();
+  const result = await request;
+  console.log("result.value", result);
+  return result;
+};
 
 initdb();
