f40afe02f5ac2a1dd88a404ebaa254e546bde0ff
Your Name <you@example.com>
2022-07-26 22:53:05 -0400

    run works

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,12 +1,13 @@
-import { openDB } from 'idb';
+import { openDB } from "idb";
 
 const initdb = async () =>
-  openDB('jate', 1, {
+  openDB( "jate", 1, {
     upgrade(db) {
-      if (db.objectStoreNames.contains('jate')) {
-        console.log('jate database already exists');
+      if (db.objectStoreName.contains('jate')) {
+        console.log('jate db already exists');
         return;
       }
+      
       db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
       console.log('jate database created');
     },
@@ -14,25 +15,29 @@ const initdb = async () =>
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (id, content) => {
-  console.log("we're in the database.");
-  const contentDb = await openDB("content", 1);
+    console.log("we're in the database.");
+    
+  const contDb = await openDB("content", 1);
   const tx = contentDb.transaction("content", "readwrite");
+
   const store = tx.objectStore("content");
+  
   const request = store.objectStore.put({ id: id, value: content });
   const result = await request;
   console.log("result.value", result);
 };
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
-export const getDb = async () => {
-  const contentDb = await openDB("content", 1);
-  const tx = contentDb.transaction("content", "readonly");
-  const store = tx.objectStore("content");
-  const request = store.objectStore.getAll();
+
+export const getDb = async (e) => {
+
+  const contDb = await openDB("jate", 1);
+  const tx = jateDb.transaction("jate", "readonly");
+  const store = tx.objectStore("jate");
+  const request = store.get(1);
   const result = await request;
-  console.log("result.value", result);
-  return result;
+  return result?.value;
+
 };
 
 initdb();
